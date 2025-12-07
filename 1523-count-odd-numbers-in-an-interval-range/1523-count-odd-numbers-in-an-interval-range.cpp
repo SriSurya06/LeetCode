7fb0ba3b7fcde9488252b8184ce164be48ebed5b
Sri Surya Lakshmi Bulusu <23MH1A05F2@acoe.edu.in>
2025-12-07 22:47:00 +0530

    Time: 2 ms (70.23%), Space: 7.6 MB (93.12%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int res;
+        if(low%2==0 && high%2==0) res=((high-low)/2);
+        else if(low%2!=0 && high%2!=0) res=((high-low)/2)+1;
+        else res=int((high-low)/2)+1;
+        return res;
+    }
+};
