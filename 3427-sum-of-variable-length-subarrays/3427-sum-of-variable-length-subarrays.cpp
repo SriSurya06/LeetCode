726ff1a6454b85a179d83fe44ccb1d71a1d9445f
Sri Surya Lakshmi Bulusu <23MH1A05F2@acoe.edu.in>
2025-12-22 16:01:33 +0530

    Time: 0 ms (100%), Space: 26.6 MB (64.63%) - LeetHub

--- 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.cpp
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.cpp
@@ -4,11 +4,9 @@ public:
         int k=0;
         for(int i=0;i<nums.size();i++){
             int l=max(0,i-nums[i]);
-            int s=0;
             for(int j=l;j<=i;j++){
-                s=s+nums[j];
+                k=k+nums[j];
             }
-            k=k+s;
         }
         return k;
     }
