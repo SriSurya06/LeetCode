ddbce5bbdb20657e002494f3c9cc489b0807bfe2
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-05-07 13:53:35 +0530

    Time: 0 ms (100%), Space: 14 MB (97.75%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0,h=nums.size()-1,m;
+        while(l<h){
+            m=(l+h)/2;
+            if(nums[m]>nums[h]) l=m+1;
+            else h=m;
+        }
+        return nums[l];
+    }
+};
