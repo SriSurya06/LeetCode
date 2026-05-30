d1ab8024feac89738befeaa362e0e703b60ebb73
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-05-30 18:01:38 +0530

    Time: 78 ms (13.78%), Space: 29.8 MB (8.74%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,14 +11,23 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if(headA==headB) return headA;
         ListNode* temp1=headA;
-        // ListNode* temp2=headB;
+        ListNode* temp2=headB;
+        // while(temp1!=NULL){
+        //     ListNode* temp2=headB;
+        //     while(temp2!=NULL){
+        //         if(temp2==temp1) return temp1;
+        //         temp2=temp2->next;
+        //     }
+        //     temp1=temp1->next;
+        // }
+        set<ListNode*> st;
         while(temp1!=NULL){
-            ListNode* temp2=headB;
-            while(temp2!=NULL){
-                if(temp2==temp1) return temp1;
-                temp2=temp2->next;
-            }
+            st.insert(temp1);
             temp1=temp1->next;
+        }
+        while(temp2!=NULL){
+            if(st.contains(temp2)) return temp2;
+            temp2=temp2->next;
         }
         return nullptr;
     }
