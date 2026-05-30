b2e729946c0dac110100ac1dd528c628a5fab7b2
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-05-30 11:20:20 +0530

    Time: 5 ms (83.6%), Space: 11.5 MB (22.05%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -11,14 +11,17 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* tor=head;
         ListNode* har=tor;
-        ListNode* temp=tor;
         while(har!=NULL && har->next!=NULL){
             tor=tor->next;
             har=har->next->next;
-            if(har==tor && har!=temp){
-                temp=temp->next;
+            if(har==tor){
+                ListNode* temp=head;
+                while(temp!=tor){
+                    temp=temp->next;
+                    tor=tor->next;
+                }
+            return temp;      
             } 
-            if(tor==temp) return temp;      
             }
         return NULL;
     }
