97c6c1893c0b75dcefeb0e09839ace050ef2ceb2
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-06-16 22:14:32 +0530

    Time: 3 ms (76.89%), Space: 39.8 MB (42.05%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -3,17 +3,12 @@ public:
     string processStr(string s) {
         string res="";
         for(int i=0;i<s.size();i++){
-            char c;
             if(s[i]>=97 && s[i]<=122){
-                c=s[i];
-                res+=c;
+                res+=s[i];
             }
-
             else if(s[i]=='*' && res.size()!=0) res.pop_back();
             else if(s[i]=='%') reverse(res.begin(),res.end());
-            else{
-                res=res+res;
-            }
+            else res=res+res;
 
         }
         return res;
