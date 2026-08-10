233c81f8a2b080910ec1a8b3515bfc17f8d031e1
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-08-10 10:13:46 +0530

    Time: 0 ms (100%), Space: 31 MB (6.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        vector<int> v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back(nums[i]);
+        }
         int n=nums.size();
         if(k>n) k=k%n;
-        vector<int> v;
-        for(int i=n-k;i<n;i++){
-            v.push_back(nums[i]);
+        int j=-1;
+        for(int i=n-k;i<nums.size();i++){
+            j++;
+            nums[j]=v[i];
         }
         for(int i=0;i<n-k;i++){
-            v.push_back(nums[i]);
+            j++;
+            nums[j]=v[i];
         }
-        nums=v;
     }
 };
