e724950f036f730ba2aa690dd3d5ca044a423132
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-08-10 11:46:24 +0530

    Time: 8 ms (97.99%), Space: 60.1 MB (98.43%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,9 +4,6 @@ public:
         if(nums.size()==0) return 0;
         int mx=1;
         int s=1;
-        // set<int> st(nums.begin(),nums.end());
-        // if(st.size()==1) return 1;
-
         sort(nums.begin(),nums.end());
         for(int i=1;i<nums.size();i++){
             if(nums[i]-nums[i-1]==0 ) continue;
