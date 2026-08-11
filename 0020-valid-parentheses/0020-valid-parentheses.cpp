55b0fcadbe1b5d388577abee3885ea932b3574f4
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-08-11 21:42:22 +0530

    Time: 0 ms (100%), Space: 9 MB (18.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     bool isValid(string s) {
         int i;
-    int st[10000];
-    int t=-1;
-    for(i=0;s[i]!='\0';i++)
-    {
-        if(t==-1)
+        int st[10000];
+        int t=-1;
+        for(i=0;s[i]!='\0';i++)
         {
-            t++;
-            st[t]=s[i];
+            if(t==-1)
+            {
+                t++;
+                st[t]=s[i];
+            }
+            else if(st[t]=='{'&&s[i]=='}' || st[t]=='['&&s[i]==']' || st[t]=='('&&s[i]==')')
+            {
+                t--;
+            }
+            else
+            {
+                t++;
+                st[t]=s[i];
+            }
         }
-        else if(st[t]=='{'&&s[i]=='}' || st[t]=='['&&s[i]==']' || st[t]=='('&&s[i]==')')
-        {
-            t--;
-        }
-        else
-        {
-            t++;
-            st[t]=s[i];
-        }
-    }
-    if(t==-1)
-    return 1;
-    else
-    return 0;
+        if(t==-1) return true;
+        else return false;
     }
 };
