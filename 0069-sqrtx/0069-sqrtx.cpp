5989d88805cb49c94d844d90ccce733ab4807e4a
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-08-18 15:03:30 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        if(x<2) return x;
+        int i=1,j=x;
+        int sq;
+        while(i<j){
+            int mid=i+(j-i)/2;
+            if(mid<=(x/mid)){
+                i=mid+1;
+                sq=mid;
+            }
+            else{
+                j=mid;
+            }
+
+        }
+        return sq;
     }
 };
