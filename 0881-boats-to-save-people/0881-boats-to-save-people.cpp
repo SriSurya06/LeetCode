772e87b46401dad86a3c2179e379d71692a742b3
B SRI SURYA LAKSHMI <23MH1A05F2@acoe.edu.in>
2026-09-26 20:20:44 +0530

    Time: 7 ms (98.9%), Space: 45.8 MB (71.45%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+     sort(people.begin(),people.end());
+        int left=0,right=people.size()-1,res=0;
+        while(left<=right){
+            if(people[left]+people[right]<=limit) left++;
+            right--;
+            res++;
+        }
+        return res;   
+    }
+};
